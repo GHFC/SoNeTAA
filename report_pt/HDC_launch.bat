6571844af6dff21a9a2a9529ab157d5d689f99f2
deep-introspection <deep@introspection.eu>
2017-08-16 18:58:07 +0200

    trying to remove folders

--- report_pt/HDC_launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting...
-cd c:\OFram\of\examples\addons\hdcxmlmicros\bin\data\
-python experimentaldg.py
-cd c:\OFram\of\examples\addons\hdcxmlmicros\bin\
-hdcxmlmicros.exe
-cd c:\OFram\of\examples\addons\hdcxmlmicros\bin\data\
-python ExtractPhase.py
-sh compile.sh
--- report_pt/compile.sh
+++
@@ -1,8 +0,0 @@
-cd c:/OFram/of/examples/addons/hdcxmlmicros/bin/data
-python compile_report.py
-cd c:/OFram/of/examples/addons/hdcxmlmicros/bin/data/report_document
-pdflatex main.tex
-rm maint.pdf
-pdflatex main.tex
-cp main.pdf c:/Users/Guillaume\ Dumas/Desktop/report.pdf
-rm maint.pdf
